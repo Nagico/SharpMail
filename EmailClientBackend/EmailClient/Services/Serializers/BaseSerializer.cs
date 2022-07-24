1bd7155ad2ec3cc23515ed50e3918918701d866b
NagisaCo <yjr888@vip.qq.com>
2022-07-24 23:15:11 +0800

    feat(auth): 添加鉴权 登录功能

---
+++ EmailClientBackend/EmailClient/Services/Serializers/BaseSerializer.cs
@@ -0,0 +1,16 @@
+﻿using EmailClient.Entities;
+
+namespace EmailClient.Services.Serializers;
+
+/// <summary>
+/// 序列化器基类
+/// </summary>
+public class BaseSerializer
+{
+    protected readonly EmailClientContext _context;
+
+    protected BaseSerializer(EmailClientContext context)
+    {
+        _context = context;
+    }
+}
---
+++ EmailClientBackend/EmailClient/ViewModels/AuthenticateViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EmailClient.ViewModels;
+
+/// <summary>
+/// 登录返回数据
+/// </summary>
+public class AuthenticateViewModel
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public bool IsActive { get; set; }
+    public string Token { get; set; }
+}
