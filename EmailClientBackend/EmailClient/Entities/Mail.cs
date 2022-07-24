4311234537d4143f55fb5261aeabe9498ac9434d
NagisaCo <yjr888@vip.qq.com>
2022-07-24 23:12:44 +0800

    feat(mail): 添加邮件模型

---
+++ EmailClientBackend/EmailClient/Entities/Mail.cs
@@ -0,0 +1,49 @@
+﻿namespace EmailClient.Entities;
+
+public class Mail
+{
+    /// <summary>
+    /// 邮件ID
+    /// </summary>
+    public int Id { get; set; }
+    
+    /// <summary>
+    /// 账户ID
+    /// </summary>
+    public int AccountId { get; set; }
+    
+    /// <summary>
+    /// 账户
+    /// </summary>
+    public Account? Account { get; set; }
+    
+    /// <summary>
+    /// 邮件类型
+    /// </summary>
+    public int? Type { get; set; }
+    
+    /// <summary>
+    /// 邮件主题
+    /// </summary>
+    public string? Subject { get; set; }
+    
+    /// <summary>
+    /// 发件人
+    /// </summary>
+    public string? From { get; set; }
+    
+    /// <summary>
+    /// 收件人
+    /// </summary>
+    public string? To { get; set; }
+    
+    /// <summary>
+    /// 邮件内容
+    /// </summary>
+    public string? Content { get; set; }
+    
+    /// <summary>
+    /// 添加时间
+    /// </summary>
+    public DateTime CreateTime { get; set; }
+}
---
+++ EmailClientBackend/EmailClient/EntityConfigs/MailConfig.cs
@@ -0,0 +1,44 @@
+﻿using EmailClient.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmailClient.EntityConfigs;
+
+public class mailConfig : IEntityTypeConfiguration<Mail>
+{
+    public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Mail> builder)
+    {
+        builder.ToTable("tb_mail");
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(x => x.Id)
+            .HasColumnName("id")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.AccountId)
+            .HasColumnName("account_id")
+            .IsRequired();
+        
+        builder.Property(x => x.Type)
+            .HasColumnName("type")
+            .IsRequired();
+
+        builder.Property(x => x.Subject)
+            .HasColumnName("subject");
+        
+        builder.Property(x => x.From)
+            .HasColumnName("from");
+        
+        builder.Property(x => x.To)
+            .HasColumnName("to");
+        
+        builder.Property(x => x.Content)
+            .HasColumnName("content");
+
+        builder.Property(x => x.CreateTime)
+            .HasColumnName("create_time");
+        
+        builder.HasOne(m => m.Account)
+            .WithMany(a => a.Mails)
+            .HasForeignKey(m => m.AccountId);
+    }
+}
