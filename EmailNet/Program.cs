7e17aa5fa055ec509f03b3af3c8de4bc64b8f2d7
NagisaCo <yjr888@vip.qq.com>
2022-07-25 18:44:55 +0800

    test(net): 测试邮件解析

--- EmailNet/Program.cs
+++ EmailNet/Program.cs
@@ -1,6 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using EmailNet;
+using MimeKit;
+
+async Task<Stream> ToStream(string? text)
+{
+    var stream = new MemoryStream();
+    var writer = new StreamWriter(stream);
+    await writer.WriteAsync(text);
+    await writer.FlushAsync();
+    stream.Position = 0;
+    return stream;
+}
 
 var email = "co_test1@163.com";
 var password = "ASSKUOTNRNQETWPU";
@@ -17,10 +28,14 @@ for (var i = 1; i <= count; i++)
 {
     var uid = await pop3Client.GetMaidUidAsync(i);
     var mail = await pop3Client.GetMailContentAsync(i);
+    var msg = await MimeMessage.LoadAsync(await ToStream(mail));
     Console.WriteLine("[{0}]\n{1}", uid, mail);
 }
 
 await pop3Client.DisconnectAsync();
+
+
+
 
 var smtpClient = new SmtpClient(email, password, smtpServer);
 await smtpClient.ConnectAsync();
